cc0593153677819e53851a30eda3cfb79ebbca5c
wang-149 <68971762+wang-149@users.noreply.github.com>
2023-01-11 23:31:25 -0500

    hangman set up

--- Demo/index.js
+++ Demo/index.js
@@ -190,7 +190,73 @@ function calculation(value){
 /// Hangman Game 
 
 // Words
+const words = [ "abruptly", "absurd", "abyss", "affix", "askew", "avenue", "awkward", "axiom", "azure", "bagpipes",
+                "bandwagon", "banjo", "bayou", "beekeeper", "bikini", "blitz","blizzard","boggle","bookworm","boxcar",
+                "boxful", "buckaroo", "buffalo", "buffoon", "buxom", "buzzard", "buzzing", "buzzwords", "caliph", "cobweb", 
+                "cockiness", "croquet", "crypt", "curacao", "cycle", "daiquiri", "dirndl", "disavow", "dizzying", "duplex",
+                "dwarves", "embezzle", "equip", "espionage", "euouae", "exodus", "faking", "fishhook", "fixable", "fjord",
+                "flapjack", "flopping", "fluffiness", "flyby", "foxglove","frazzled", "frizzled", "fuchsia", "funny", "gabby",
+                "galaxy", "galvanize", "gazebo", "giaour", "gizmo", "glowworm", "glyph", "gnarly", "gnostic", "gossip", 
+                "grogginess", "haiku", "haphazard", "hyphen", "iatrogenic", "icebox", "injury", "ivory", "ivy", "jackpot",
+                "jaundice", "jawbreaker", "jaywalk", "jazziest", "jazzy", "jelly", "jigsaw", "jinx", "jiujitsu", "jockey", 
+                "jogging", "joking", "jovial", "joyful", "juicy", "jukebox", "jumbo", "kayak", "kazoo", "keyhole",
+                "khaki", "kilobyte", "kiosk", "kitsch", "kiwifruit", "klutz", "knapsack", "larynx", "lengths", "lucky", "luxury",
+                "lymph", "marquis", "matrix", "megahertz", "microwave", "mnemonic", "mystify", "naphtha", "nightclub", "nowadays",
+                "numbskull", "nymph", "onyx", "ovary", "oxidize", "oxygen", "pajama", "peekaboo", "phlegm", "pixel",
+                "pizazz", "pneumonia", "polka", "pshaw", "psyche", "puppy", "puzzling", "quartz","queue","quips",
+                "quixotic", "quiz", "quizzes", "quorum", "razzmatazz", "rhubarb", "rhythm", "rickshaw", "schnapps", "scratch",
+                "shiv", "snazzy", "sphinx", "spritz", "squawk", "staff", "strength", "strengths", "stretch", "stronghold", 
+                "stymied", "subway", "swivel", "syndrome", "thriftless", "thumbscrew", "topaz", "transcript", "transgress", "transplant",
+                "triphthong", "twelfth", "twelfths", "unknown", "unworthy", "unzip", "uptown", "vaporize", "vixen", "vodka",
+                "voodoo", "vortex", "voyeurism", "walkway", "waltz", "wave", "wavy", "waxy", "wellspring", "wheezy", 
+                "whiskey", "whizzing", "whomever", "wimpy", "witchcraft", "wizard", "woozy", "wristwatch", "wyvern", "xylophone",
+                "yachtsman", "yippee", "yoked", "youthful", "yummy", "zephyr", "zigzag", "zigzagging", "zilch", "zipper", "zodiac", "zombie"];
 
-const words = [];
+let word = "";
 
+let missLetter = "";
+// DOM elements
+
+const image = document.getElementById("image");
+const wordDiv = document.getElementById("word");
+const missesDiv = document.getElementById("misses");
+let target = "";
+
+// image array 
+
+const imgs = ["Hangman-0.png", "Hangman-1.png", "Hangman-2.png", "Hangman-3.png", "Hangman-4.png", "Hangman-5.png", "Hangman-6.png"]
+
+
+// Attempt variable
 let attemptLeft = 6;
+
+let attempt = 0;
+
+// Generate a word to play with 
+function generate(){
+    let randNum = Math.floor(Math.random() * words.length);
+    console.log(randNum);
+    target = words[randNum];
+    console.log(target);
+}
+
+// Hangman function
+
+function hangman(){
+
+    const char = document.getElementById("guess").value;
+    console.log(char);
+    document.getElementById("guess").value = ""; 
+
+    // Check the target string. Does it contain the char value?
+    if(target.includes(char)){
+        // Do something
+    }
+    else {
+        attempt ++;
+        missLetter = missLetter + " " + char;
+        image.setAttribute("src",imgs[attempt]);
+
+        missesDiv.innerText = missLetter;
+    }
+}
