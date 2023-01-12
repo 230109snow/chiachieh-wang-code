be2d0ac6967560789a412d0bacd1ebe0444d4461
wang-149 <68971762+wang-149@users.noreply.github.com>
2023-01-12 17:35:30 -0500

    Fetch ok

--- Api/index.js
+++ Api/index.js
@@ -5,8 +5,23 @@ function fetchData() {
         'x-api-key': "live_JnDJxEA1DCW2HgQEte7GlLzx1dphr0XGNQ0qiMMMHy2yZjQ53hbZN3q44lD1xg9s"
       }}).then((response) => response.json()).then((data) => {
         console.log(data);
-     
+        const divField = document.getElementById("container");
+
+        const img = document.createElement("img");
+        img.width = 300;
+        img.src=data[0].url;
+
+        divField.appendChild(img);
+
     })
  
   
+}
+
+async function fetchD(){
+  const url = `https://api.thecatapi.com/v1/images/search`;
+  const response = await fetch(url);
+  const data = response.json();
+
+  console.log(data);
 }
