33b534cb5aed7dfce186650927f1ad7138196d0b
wang-149 <68971762+wang-149@users.noreply.github.com>
2023-01-18 16:40:22 -0500

    Fix images problem

--- angular/ng-demo/src/app/app.component.ts
+++ angular/ng-demo/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-demo';
+  path: string = "assets/images/Hangman0.png";
+  alttext: string="NO IMAGE"
 }
--- angular/ng-demo/src/app/hangman/hangman.component.ts
+++ angular/ng-demo/src/app/hangman/hangman.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class HangmanComponent {
 
+  imgSrc = "../../assets/images/Hangman0.png";
+
+
 }
