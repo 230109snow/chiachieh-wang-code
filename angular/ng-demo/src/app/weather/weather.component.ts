ef4c45a7aab6be7709e6da4ec1b0454c34fda840
wang-149 <68971762+wang-149@users.noreply.github.com>
2023-01-19 15:42:45 -0500

    get data successful

--- angular/ng-demo/src/app/weather/weather.component.ts
+++ angular/ng-demo/src/app/weather/weather.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+import { apikey } from './weatherKey';
 
 @Component({
   selector: 'app-weather',
@@ -6,16 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent {
+  constructor(private http: HttpClient) {}
   cityName : string = "";
 
 
 
   getData(){
+
    // this.cityName = (<HTMLInputElement>document.getElementById("city")).value ;
     
     console.log(this.cityName);
     console.log("Get data function called");
     /**https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} */
+    // API Key = "55eb1dc1b9f70cc7e36c5a220ea1d9ab"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.cityName}&appid=${apikey}&units=imperial`;
+    this.http.get(url).subscribe( (data) => {
+      console.log(data);
+    })
   }
 
 }
