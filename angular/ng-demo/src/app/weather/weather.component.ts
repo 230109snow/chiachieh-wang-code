f9bac2331d832a95c2e95901ae27e4c729f06c2a
wang-149 <68971762+wang-149@users.noreply.github.com>
2023-01-19 18:40:37 -0500

    adjustment weather

--- angular/ng-demo/src/app/weather/weather.component.ts
+++ angular/ng-demo/src/app/weather/weather.component.ts
@@ -11,10 +11,13 @@ import { apikey } from './weatherKey';
 export class WeatherComponent {
   constructor(private http: HttpClient) {}
   cityName : string = "";
+  weatherObj  : object[] = [];
 
-
-
-  getData(){
+  lat : number = 0;
+  lon : number = 0;
+  getData(){  
+    console.log("getData function called")
+   
 
    // this.cityName = (<HTMLInputElement>document.getElementById("city")).value ;
     
@@ -23,9 +26,69 @@ export class WeatherComponent {
     /**https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} */
     // API Key = "55eb1dc1b9f70cc7e36c5a220ea1d9ab"
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.cityName}&appid=${apikey}&units=imperial`;
-    this.http.get(url).subscribe( (data) => {
+    this.http.get(url).subscribe( (data : any) => {
       console.log(data);
+
+      const weatherDetail = {
+        city : data.name,
+        country  : data.sys.country,
+        temp : data.main.temp,
+        feel : data.main.feels_like,
+        humidity : data.main.humidity,
+        pressure : data.main.pressure,
+        icon : data.weather[0].icon,
+        description : data.weather[0].description,   
+        windSpeed : data.wind.speed
+      }
+
+
+      console.log(weatherDetail);
+
+      this.weatherObj.push(weatherDetail);
+
+      // console.log(this.weatherObj[0]);
+      // this.lat = data.coord.lat;
+      // this.lon = data.coord.lon;
+
+      // console.log("lat : " + this.lat + " \nlon : " +this.lon);
+
+      // this.getforecast(this.lat, this.lon)
+      //  const obj : any = data;
+      // // console.log( typeof data)
+      // console.log(data.main.temp)
+      // this.weatherObj.push(data);
+      
+      // // console.log(this.weatherObj.length)
+      // // console.log(this.weatherObj)
+
+      // console.log(obj.sys.country)
+      // console.log(this.weatherObj[0].sys.country)
+
+    
     })
   }
-
+  // getforecast(lat: number, lon: number) {
+  //   console.log("getForecast function called")
+  //   const fUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&&appid=${apikey}&units=imperial`;
+  //   this.http.get(fUrl).subscribe( (data : any) => {
+  //     console.log(data);
+     
+     
+  //     //  const obj : any = data;
+  //     // // console.log( typeof data)
+  //     // console.log(data.main.temp)
+  //     // this.weatherObj.push(data);
+      
+  //     // // console.log(this.weatherObj.length)
+  //     // // console.log(this.weatherObj)
+  
+  //     // console.log(obj.sys.country)
+  //     // console.log(this.weatherObj[0].sys.country)
+  
+    
+  //   })
+  // }
 }
+
+
+
