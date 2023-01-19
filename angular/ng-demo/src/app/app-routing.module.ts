d130ea9a38b881c9ceb0b1622a8f3aeb734303e0
wang-149 <68971762+wang-149@users.noreply.github.com>
2023-01-19 14:10:20 -0500

    Set up nav bar properly

--- angular/ng-demo/src/app/app-routing.module.ts
+++ angular/ng-demo/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { FetchCatComponent } from './fetch-cat/fetch-cat.component';
+import { HangmanComponent } from './hangman/hangman.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'calculator',
+    component : CalculatorComponent
+  },
+  {
+    path : 'hangman',
+    component : HangmanComponent
+  },
+  {
+    path : 'cats',
+    component : FetchCatComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
